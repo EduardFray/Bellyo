2ba12ae5f792d2d352d76c882432aae8cf2cfe9f
Eduard Fray <eduard.fray@hotmail.de>
2025-08-27 15:52:27 +0200

    logo plus hero section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { SidenavComponent } from "./sidenav/sidenav.component";
+import { MainContentComponent } from './main-content/main-content.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, SidenavComponent],
+  imports: [RouterOutlet, HeaderComponent, SidenavComponent, MainContentComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
---
+++ src/app/hero-section/hero-section.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
+
+@Component({
+  selector: 'app-hero-section',
+  imports: [MatIconModule],
+  templateUrl: './hero-section.component.html',
+  styleUrls: ['./hero-section.component.scss']
+})
+export class HeroSectionComponent {
+
+}
